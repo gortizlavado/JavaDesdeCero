24b457c0bf82921f50bef6ec8a837e02517b27ef
killeronline72 <jfherrero05@gmail.com>
2024-07-30 18:25:59 +0200

    Lebron con shoot y score

--- classes/sport/basket/LebronJames.java
+++ classes/sport/basket/LebronJames.java
@@ -4,6 +4,36 @@ public class LebronJames {
     public static void main(String[] args) {
         Pelota willson = new Pelota();
         System.out.println("Im the king y tengo la pelota");
+        Integer distance= new Integer(7);
+        score(distance);
     }
+
+    public static void score() {
+        //String points= new String("3+1");
+        Integer points= new Integer(3);
+        shoot(points);
+        System.out.println("Celebra");
+    }
+
+    private static void shoot(String points) {
+        System.out.println(points + "puntos");
+    }
+
+    private static void shoot(Integer points) {
+        System.out.println(points + "puntos");
+    }
+
+    public static void score(Integer distance) {
+        System.out.println(distance + "m");
+        if (distance > 10) {
+            System.out.println(3 + "puntos");
+        } else {
+            System.out.println(2 + "puntos");
+        }
+        System.out.println("Celebra");
+
+    }
+
+
 }
 
