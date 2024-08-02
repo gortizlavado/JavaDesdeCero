5f328a3bb97df332a75a2254749e5971002c6eef
killeronline72 <jfherrero05@gmail.com>
2024-08-02 13:01:29 +0200

    Clase pelota actualizada

--- classes/sport/basket/Pelota.java
+++ classes/sport/basket/Pelota.java
@@ -1,7 +1,22 @@
 package sport.basket;
 
-class Pelota{
+class Pelota {
     public static void main(String[] args) {
+        botar();
         System.out.println("Estoy botando");
+        encestar(4);
+    }
+
+    public static void botar() {
+        Integer bounce = new Integer(3);
+        System.out.println("He dado " + bounce + " botes");
+    }
+
+    public static void encestar(Integer bounce) {
+        if (bounce >= 4) {
+            System.out.println("Has encestado");
+        } else {
+            System.out.println("No has encestado");
+        }
     }
 }
