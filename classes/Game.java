0f45dd54f1e7641230307008750c0a785fe738bc
Gonzalo Ortiz <gonzalo.ortiz.lavado@protonmail.com>
2024-08-06 17:33:37 +0200

    playing with loop

--- classes/Game.java
+++ classes/Game.java
@@ -1,5 +1,4 @@
 import sport.basket.Canasta;
-import sport.basket.LebronJames;
 
 import static sport.basket.LebronJames.score;
 
@@ -9,27 +8,40 @@ public class Game {
         Canasta canasta = new Canasta();
         Canasta canastaLJ = new Canasta();
         Integer distance = 8;
-        //score(distance);
-        canasta.refreshScore(score(distance));
+        Integer points = score(distance);
+        for (int i=0; i < 5; i++) {
+            System.out.println("Possession " + (i + 1));
+            if (i % 2 == 0) {
+                canasta.refreshScore(points);
+            } else {
+                canastaLJ.refreshScore(3);
+            }
+            muestraDelMarcador(canasta,canastaLJ);
+            System.out.println();
+        }
 
-        muestraDelMarcador(canasta,canastaLJ);
-
-        canastaLJ.refreshScore(3);
-
-        muestraDelMarcador(canasta,canastaLJ);
-//Lebron triple
-        canasta.refreshScore(3);
-
-        muestraDelMarcador(canasta,canastaLJ);
-
-        canastaLJ.refreshScore(2);
-
-        muestraDelMarcador(canasta,canastaLJ);
-
+        //playWithForLoop();
     }
 
     public static void muestraDelMarcador(Canasta canasta,Canasta canastaLJ) {
         System.out.println("Marcador LJ " + canasta.getScore() + " puntos");
         System.out.println("Nuestro marcador " + canastaLJ.getScore() + " puntos");
+    }
+
+    public static void playWithForLoop() {
+        System.out.println("Playing with loop 'for'");
+        for (int i=0; i<5; i++) {
+            System.out.println(i);
+        }
+
+        String[] arrayString = {"a", "b", "c"};
+        System.out.println("position zero: " + arrayString[0]);
+        System.out.println("position one: " + arrayString[1]);
+        System.out.println("position two: " + arrayString[2]);
+
+        System.out.println("------ BUCLE ------");
+        for (int i=0; i < 3; i++) {
+            System.out.println(arrayString[i]);
+        }
     }
 }
