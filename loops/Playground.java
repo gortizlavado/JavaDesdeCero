4dee6e9e89bd0edeed5be98818ef1e83d7dd521a
Gonzalo Ortiz <gonzalo.ortiz.lavado@protonmail.com>
2024-08-08 17:35:30 +0200

    new class to play with loops

---
+++ loops/Playground.java
@@ -0,0 +1,42 @@
+package loops;
+
+public class Playground {
+    public static void main(String[] args) {
+        System.out.println("Playground for looping exercises");
+
+        String text = "12345";
+        printReverseText(text);
+
+    }
+
+    private static void printReverseText(String textToReverse) {
+        char[] charArray = textToReverse.toCharArray();
+        int length = charArray.length;
+        for (int i = length-1; i > -1; i--) {
+            //#1 i = 5-1 = 4 -> charArray[0] = 1 ; ... ; charArray[4] = 5
+            //#2 i = 3 ->  charArray[3] = 4
+            System.out.println("index: " + i + " --> " + charArray[i]);
+        }
+    }
+
+    private static String reverseText(String textToReverse) {
+        String result = new String();
+
+        return result; //"54321"
+    }
+}
+
+/*
+#1
+Write a program that reads a set of characters like 12345 and then outputs the number with the digits reversed. The output should be 54321.
+
+#2
+Write a program that reads a set of integers, and then prints the sum of the even and odd integers.
+
+#3
+Write a program that prompts the user to input a positive integer. It should then output a message indicating whether the number is a prime number. If not the program ask for another number.
+Tip: Scanner in = new Scanner(System.in);
+
+#4
+Write a program to enter the numbers till the user wants and at the end the program should display the biggest and smallest numbers entered.
+*/
