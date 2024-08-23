a57bb90b1f3cc31bd3249c2d684b418343c1f312
Gonzalo Ortiz <gonzalo.ortiz.lavado@protonmail.com>
2024-08-23 17:03:45 +0200

    new class for practicing

---
+++ src/datatypes/Calculator.java
@@ -0,0 +1,38 @@
+package datatypes;
+
+public class Calculator {
+
+    private String result;
+
+    public String result() {
+        return result;
+    }
+
+    public void addition(int x, int y) {
+        //TODO
+    }
+
+    public void additionOne(int x) {
+        //TODO
+    }
+
+    public void calculateTriangleArea(int base, int height) {
+        //TODO
+    }
+
+    public void convertSecondsToHours(int seconds) {
+        //TODO
+    }
+
+    public void convertAgesToDays(int ages) {
+        //TODO
+    }
+
+    public void uppercase(String text) {
+        //TODO
+    }
+
+    public void findInTheText(String toFind, String text) {
+        //TODO
+    }
+}
