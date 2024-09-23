98167c7c20975f08268718d6ea49b5912bc59571
killeronline72 <jfherrero05@gmail.com>
2024-09-23 18:37:47 +0200

    quadraticEquation

--- src/datatypes/ArrancarCalculator.java
+++ src/datatypes/ArrancarCalculator.java
@@ -3,7 +3,7 @@ package datatypes;
 public class ArrancarCalculator {
     public static void main(String args[]){
        Calculator casio = new Calculator();
-       casio.convertDaysToAges(5);
+       casio.solveQuadraticEquation("57","-97","2");
         String resultado = casio.result();
         System.out.println("el resultado es "+ resultado );
     }
