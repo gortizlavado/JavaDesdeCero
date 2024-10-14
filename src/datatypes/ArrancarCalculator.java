efc0823f77c58a4d4b4a1a8494575b23af76467c
Gonzalo Ortiz <gonzalo.ortiz.lavado@protonmail.com>
2024-10-14 18:24:08 +0200

    first time using scanner.

--- src/datatypes/ArrancarCalculator.java
+++ src/datatypes/ArrancarCalculator.java
@@ -1,10 +1,22 @@
 package datatypes;
 
+import java.util.Scanner;
+
 public class ArrancarCalculator {
     public static void main(String args[]){
-       Calculator casio = new Calculator();
-       casio.solveQuadraticEquation("57","-97","2");
+        Calculator casio = new Calculator();
+
+        /*
+        casio.solveQuadraticEquation("57","-97","2");
         String resultado = casio.result();
         System.out.println("el resultado es "+ resultado );
+         */
+
+        System.out.println("Hi! this is a program that catch a text and a number");
+        Scanner input = new Scanner(System.in);
+        String s = input.next();
+        int i = input.nextInt();
+        System.out.println("I get: " + s + " " + i + " from console");
+
     }
 }
