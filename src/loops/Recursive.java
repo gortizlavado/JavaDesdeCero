17922cd54e256858e9867b2c525cc94731c9f63f
Gonzalo Ortiz <gonzalo.ortiz.lavado@protonmail.com>
2025-02-19 18:11:16 +0100

    new example of recursive

--- src/loops/Recursive.java
+++ src/loops/Recursive.java
@@ -6,6 +6,23 @@ public class Recursive {
 
     public static void main(String[] args) {
         readAbecedary(0);
+        System.out.println();
+        System.out.println("--------------");
+        counterBack(10);
+        System.out.println();
+        System.out.println("--------------");
+        int factorial=factorailOf(5);
+        System.out.print(factorial);
+    }
+
+    private static void counterBack(int i) {
+        if (i == -1) {
+            return;
+        }
+
+        System.out.print(i + " ");
+        //i--;
+        counterBack(--i);
     }
 
     private static void readAbecedary(int i) {
@@ -15,7 +32,7 @@ public class Recursive {
 
         char[] array = ABECEDARY.toCharArray();
         char c = array[i];
-        System.out.println(c);
+        System.out.print(c + " ");
         i++;
         readAbecedary(i);
     }
