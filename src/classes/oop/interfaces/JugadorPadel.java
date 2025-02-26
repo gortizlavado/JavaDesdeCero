00a73c28f5836aac73d673776b21c2b05abf9d57
killeronline72 <jfherrero05@gmail.com>
2025-02-26 17:56:27 +0100

    fibonacci cabron

--- src/classes/oop/interfaces/JugadorPadel.java
+++ src/classes/oop/interfaces/JugadorPadel.java
@@ -1,6 +1,8 @@
 package classes.oop.interfaces;
 
 public class JugadorPadel implements Jugador, Persona {
+    private int puntos;
+
     @Override
     public void competir() {
 
@@ -19,5 +21,14 @@ public class JugadorPadel implements Jugador, Persona {
     @Override
     public void enfadar() {
 
+    }
+    private String remate(String tipo){
+        if (tipo =="x3"){
+            System.out.println("La he sacado por 3");
+        }
+        if (tipo=="x4"){
+            System.out.println("La he sacado por 4 y he ganado el punto directo");
+            puntos++;
+        }
     }
 }
